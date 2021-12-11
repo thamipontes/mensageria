9b07e0152695806bc5430a8e3cedd26720cc70fd
Thamires <thamiresdepontesdasilva@gmail.com>
2021-12-11 18:39:17 -0300

    :tada: Projeto

---
+++ src/test/java/com/microservice/mensageria/MensageriaApplicationTests.java
@@ -0,0 +1,11 @@
+package com.microservice.mensageria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MensageriaApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
