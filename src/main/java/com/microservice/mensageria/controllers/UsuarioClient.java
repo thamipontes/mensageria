b5b44e4f4a1ad51a204038acdff8145671975960
Thamires <thamiresdepontesdasilva@gmail.com>
2021-12-19 14:01:56 -0300

    Heroku config

--- src/main/java/com/microservice/mensageria/controllers/UsuarioClient.java
+++ src/main/java/com/microservice/mensageria/controllers/UsuarioClient.java
@@ -6,7 +6,7 @@ import java.util.List;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name = "usuarioClient", url = "http://localhost:8070", configuration = UsuarioClientConfig.class)
+@FeignClient(name = "usuarioClient", url = "https://folha-ponto-backend.herokuapp.com", configuration = UsuarioClientConfig.class)
 public interface UsuarioClient {
     @GetMapping(value = "/usuario")
     List<UsuarioDTO> pegarTodosUsuarioClient();
